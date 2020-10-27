3766b7907d5a4c81028af81b5813b7c01e49a948
ashkan65 <ashkan.pourkand@gmail.com>
2020-10-27 01:21:09 -0600

    Need to finish the sensoray

--- main.cpp
+++ main.cpp
@@ -22,7 +22,22 @@
 // #include <opencv2/viz.hpp>
 // #include "print.h"  // This has the functions to print the camera and library version. TODO: move it somewhere else: 
 
+// Helpful macros for DIOs
+#define DIO(C)                  ((uint64)1 << (C))                          // convert dio channel number to uint64 bit mask
+#define DIOMASK(N)              {(uint)(N) & 0xFFFFFF, (uint)((N) >> 24)}   // convert uint64 bit mask to uint[2] array
+#define DIOSTATE(STATES,CHAN)   ((STATES[CHAN / 24] >> (CHAN % 24)) & 1)    // extract dio channel's boolean state from uint[2] array
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ERROR HANDLING
+// These examples employ very simple error handling: if an error is detected, the example functions will immediately return an error code.
+// This behavior may not be suitable for some real-world applications but it makes the code easier to read and understand. In a real
+// application, it's likely that additional actions would need to be performed. The examples use the following X826 macro to handle API
+// function errors; it calls an API function and stores the returned value in errcode, then returns immediately if an error was detected.
+
+#define X826(FUNC)   if ((errcode = FUNC) != S826_ERR_OK) { printf("\nERROR: %d\n", errcode); return errcode;}
+
+int errcode;
 
 int main () {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -97,27 +112,29 @@ int main () {
 		cam2.UpdateFrame();
         cam1.FilterCurrentFrame();
         cam2.FilterCurrentFrame();
-        std::cout <<"This is one frame : ";
+        // std::cout <<"This is one frame : ";
         Pt1 = cam1.GetTargetPose();
         Pt2 = cam2.GetTargetPose();
         cv::Mat Taret1 = (cv::Mat_<double>(2,1) << Pt1.x, Pt1.y);
         cv::Mat Taret2 = (cv::Mat_<double>(2,1) << Pt2.x, Pt2.y);
         
 
-		cv::triangulatePoints(Projection1,Projection2,Taret1,Taret2,pnts3D);
-        point3D = pnts3D;
-        point3D.at<double>(0, 0) = point3D.at<double>(0, 0)/pnts3D.at<double>(3, 0);
-        point3D.at<double>(1, 0) = point3D.at<double>(1, 0)/pnts3D.at<double>(3, 0);
-        point3D.at<double>(2, 0) = point3D.at<double>(2, 0)/pnts3D.at<double>(3, 0);
-        std::cout << point3D<<std::endl;
+		// cv::triangulatePoints(Projection1,Projection2,Taret1,Taret2,pnts3D);
+  //       point3D = pnts3D;
+  //       point3D.at<double>(0, 0) = point3D.at<double>(0, 0)/pnts3D.at<double>(3, 0);
+  //       point3D.at<double>(1, 0) = point3D.at<double>(1, 0)/pnts3D.at<double>(3, 0);
+  //       point3D.at<double>(2, 0) = point3D.at<double>(2, 0)/pnts3D.at<double>(3, 0);
+  //       std::cout << point3D<<std::endl;
 
-        std::cout << point3D<<std::endl;
+  //       std::cout << point3D<<std::endl;
         cv::imshow("image1", cam1.GetCurrentFrame()); 
         // cv::imshow("Object Detection1", cam1.GetCurrentFilteredFrame());
         // cv::imshow("Object Detection2", cam2.GetCurrentFilteredFrame());
         cv::imshow("image2", cam2.GetCurrentFrame());
+        X826( S826_DacDataWrite(0, 0, 32767, 0));
         key = cv::waitKey(30);        
     }
+    X826( S826_DacDataWrite(0, 0, 32767, 0));
     return 0;
 }
 
