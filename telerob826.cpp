66993a629d4a2a709b8fc474efa0ebbc089836ed
ashkan65 <ashkan.pourkand@gmail.com>
2020-10-27 14:44:30 -0600

    The analog write works not+ .hpp and .cpp for Tele826 lib--cmake works fine

---
+++ telerob826.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+#include "telerob826.hpp"
+
--- telerob826.hpp
+++ telerob826.hpp
@@ -22,16 +22,16 @@ class Telerob826{
 	public:
 		Telerob826();
 		~Telerob826();
-		Telerob826(); 
+		// Telerob826(); 
 		int Initial();
 		void PrintCardInfo();
 		void UpdateD2A(); //Updates the frame
 		void SetD2A();
-		cv::Mat GetCurrentD2A();	//Returns the current frame from the camera (Type : opencv Mat)
-		cv::Mat GetCurrentA2D();	//Returns the current frame from the camera (Type : opencv Mat)
-		cv::Mat FilterSetup();	//Returns the current frame from the camera (Type : opencv Mat)
-		cv::Mat FilterSignal();
-		cv::Point GetTargetPose();
+		// cv::Mat GetCurrentD2A();	//Returns the current frame from the camera (Type : opencv Mat)
+		// cv::Mat GetCurrentA2D();	//Returns the current frame from the camera (Type : opencv Mat)
+		// cv::Mat FilterSetup();	//Returns the current frame from the camera (Type : opencv Mat)
+		// cv::Mat FilterSignal();
+		// cv::Point GetTargetPose();
 		void SaveCurrentFrame(std::string fileName); // Updates and saves the current frame to the filename. You can draw the coordiante for the marker and also add the markers ID for ALL OF THE IDs.   
 		void FilterCurrentFrame(); // This is a place holder for the filter process if case you want to implement something later   
 };
