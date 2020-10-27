25447ac8927b20fb23fbce660b2c7e5785e9c05a
ashkan65 <ashkan.pourkand@gmail.com>
2020-10-27 14:25:16 -0600

    The analog write works not+ .hpp for Tele826 lib

--- main.cpp
+++ main.cpp
@@ -19,8 +19,6 @@
 #include "826api.h"
 #endif
 
-// #include <opencv2/viz.hpp>
-// #include "print.h"  // This has the functions to print the camera and library version. TODO: move it somewhere else: 
 
 // Helpful macros for DIOs
 #define DIO(C)                  ((uint64)1 << (C))                          // convert dio channel number to uint64 bit mask
@@ -28,118 +26,125 @@
 #define DIOSTATE(STATES,CHAN)   ((STATES[CHAN / 24] >> (CHAN % 24)) & 1)    // extract dio channel's boolean state from uint[2] array
 
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// ERROR HANDLING
-// These examples employ very simple error handling: if an error is detected, the example functions will immediately return an error code.
-// This behavior may not be suitable for some real-world applications but it makes the code easier to read and understand. In a real
-// application, it's likely that additional actions would need to be performed. The examples use the following X826 macro to handle API
-// function errors; it calls an API function and stores the returned value in errcode, then returns immediately if an error was detected.
 
 #define X826(FUNC)   if ((errcode = FUNC) != S826_ERR_OK) { printf("\nERROR: %d\n", errcode); return errcode;}
 
-int errcode;
 
-int main () {
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int GetDacOutput(uint board, uint chan, double *volts)
+{
+  uint range, setpoint;
+  int errcode = S826_DacRead(board, chan, &range, &setpoint, 0);     // Get DAC output range & setpoint.
+  std::cout<<"Reading:"<<setpoint;
+  std::cout<<range<<std::endl;
+  switch (range) {                                                   // Convert binary setpoint to volts:
+    case S826_DAC_SPAN_0_5:   *volts = setpoint            * ( 5.0 / 0xFFFF); break;  // 0 to +5V
+    case S826_DAC_SPAN_0_10:  *volts = setpoint            * (10.0 / 0xFFFF); break;  // 0 to +10V
+    case S826_DAC_SPAN_5_5:   *volts = (setpoint - 0x8000) * ( 5.0 / 0xFFFF); break;  // -5V to +5V
+    case S826_DAC_SPAN_10_10: *volts = (setpoint - 32767.0) * (20.0 / 65535.0); break;  // -10V to +10V
+  }
+  return errcode;
+}
 
+#define PI  3.1415926535
 
-    //Setup for the Cameras: --- DON'T MODIFIY ---
-    FlyCapture2::Error error;      
-	FlyCapture2::BusManager busMgr;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	unsigned int numCameras;
-    error = busMgr.GetNumOfCameras(&numCameras);
-    if (error != FlyCapture2::PGRERROR_OK)
-    {
-        error.PrintErrorTrace();
-        std::cout<< "something is wrong!\n"; 
-        return -1;
-    }
-	std::cout << "Number of cameras detected: " << numCameras << std::endl;
-	FlyCapture2::Camera *pCameras = new FlyCapture2::Camera[numCameras];
-	for (unsigned int i = 0; i < numCameras; i++)
-    {
-        FlyCapture2::PGRGuid guid;
-        error = busMgr.GetCameraFromIndex(i, &guid);
-        if (error != FlyCapture2::PGRERROR_OK)
-        {
-            error.PrintErrorTrace();
-            std::cout << "Press Enter to exit." << std::endl;
-            delete[] pCameras;
-            std::cin.ignore();
-            return -1;
-        }
+int SetDacOutput(uint board, uint chan, uint range, double volts)
+{
+  uint setpoint;
+  std::cout<<range<<std::endl;
+  switch (range) {  // conversion is based on dac output range:
+    case S826_DAC_SPAN_0_5:   setpoint = (int)(volts * 0xFFFF /  5);          break; // 0 to +5V
+    case S826_DAC_SPAN_0_10:  setpoint = (int)(volts * 0xFFFF / 10);          break; // 0 to +10V
+    case S826_DAC_SPAN_5_5:   setpoint = (int)(volts * 0xFFFF / 10) + 0x8000; break; // -5V to +5V
+    case S826_DAC_SPAN_10_10: setpoint = (int)(volts * 0xFFFF / 20) + 0x8000; break; // -10V to +10V
+    default:                  return S826_ERR_VALUE;                                  // invalid range
+  }
+    std::cout<<"Writing:"<<setpoint;
 
-        // Connect to a camera
-        error = pCameras[i].Connect(&guid);
-        if (error != FlyCapture2::PGRERROR_OK)
-        {
-            error.PrintErrorTrace();
-            delete[] pCameras;
-            std::cout << "Press Enter to exit." << std::endl;
-            std::cin.ignore();
-            return -1;
-        }
-        std::cout<<i;	
-    }
-	///////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    cv::FileStorage cal_file("../../../CalibrationMaster/build/cam_stereo.yml", cv::FileStorage::READ); // This is the location for the calibration file!
-    cv::Mat cameraMatrix1, cameraMatrix2, Projection1, Projection2, Rect1, Rect2, distCoeffs1, distCoeffs2; 
-    cal_file["Camera Matrix1"] >> cameraMatrix1;
-    cal_file["Camera Matrix2"] >> cameraMatrix2;
-    cal_file["Projection Matrix1"] >> Projection1;
-    cal_file["Projection Matrix2"] >> Projection2;
-    cal_file["Rectification Matrix1"] >> Rect1;
-    cal_file["Rectification Matrix2"] >> Rect2;
-    cal_file["Dist Coeffs1"] >> distCoeffs1;
-    cal_file["Dist Coeffs2"] >> distCoeffs2;
-// std::cout<< Rect2;
-    cv::Mat pnts3D(1,1,CV_64F);
-    cv::Mat point3D(1,1,CV_64F);
-
-    //////////////////////////////////////////////
-	TelerobCamera cam1(pCameras[0]);
-	TelerobCamera cam2(pCameras[1]);
-
-    cv::Point Pt1, Pt2;
-	char key = 0;
-    while(key != 'q')
-    {
-		cam1.UpdateFrame();
-		cam2.UpdateFrame();
-        cam1.FilterCurrentFrame();
-        cam2.FilterCurrentFrame();
-        // std::cout <<"This is one frame : ";
-        Pt1 = cam1.GetTargetPose();
-        Pt2 = cam2.GetTargetPose();
-        cv::Mat Taret1 = (cv::Mat_<double>(2,1) << Pt1.x, Pt1.y);
-        cv::Mat Taret2 = (cv::Mat_<double>(2,1) << Pt2.x, Pt2.y);
-        
-
-		// cv::triangulatePoints(Projection1,Projection2,Taret1,Taret2,pnts3D);
-  //       point3D = pnts3D;
-  //       point3D.at<double>(0, 0) = point3D.at<double>(0, 0)/pnts3D.at<double>(3, 0);
-  //       point3D.at<double>(1, 0) = point3D.at<double>(1, 0)/pnts3D.at<double>(3, 0);
-  //       point3D.at<double>(2, 0) = point3D.at<double>(2, 0)/pnts3D.at<double>(3, 0);
-  //       std::cout << point3D<<std::endl;
-
-  //       std::cout << point3D<<std::endl;
-        cv::imshow("image1", cam1.GetCurrentFrame()); 
-        // cv::imshow("Object Detection1", cam1.GetCurrentFilteredFrame());
-        // cv::imshow("Object Detection2", cam2.GetCurrentFilteredFrame());
-        cv::imshow("image2", cam2.GetCurrentFrame());
-        X826( S826_DacDataWrite(0, 0, 32767, 0));
-        key = cv::waitKey(30);        
-    }
-    X826( S826_DacDataWrite(0, 0, 32767, 0));
-    return 0;
+  return S826_DacDataWrite(board, chan, setpoint, 0);  // program DAC output and return error code
+  // return S826_DacRawWrite(board, chan, setpoint);  // program DAC output and return error code
 }
 
 
 
 
-//////////////////////////////////
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Main function.
+
+int main(int argc, char **argv)
+{
+    uint board      = 0;                        // change this if you want to use other than board number 0
+    int errcode     = S826_ERR_OK;  
+    int boardflags  = S826_SystemOpen();        // open 826 driver and find all 826 boards
+    if (argc > 1)
+        board = atoi(argv[1]);
+
+    if (boardflags < 0)
+        errcode = boardflags;                       // problem during open
+    else if ((boardflags & (1 << board)) == 0) {
+        int i;
+        printf("TARGET BOARD of index %d NOT FOUND\n",board);         // driver didn't find board you want to use
+        for (i = 0; i < 8; i++) {
+            if (boardflags & (1 << i)) {
+                printf("board %d detected. try \"./s826demo %d\"\n", i, i);
+            }
+        }
+    } else  {
+        // Execute the demo functions. Uncomment any functions you want to run.
+        S826_DacRangeWrite(0, 0, S826_DAC_SPAN_10_10, 1);
+        int errcode = SetDacOutput(0, 0, S826_DAC_SPAN_10_10, 10.00);
+
+        // X826( DemoSinewaveGenerator(board)  );      // analog sinewave output
+        // X826( TestDacRW(board) );
+        double volts;
+        if (GetDacOutput(0, 0, &volts) == S826_ERR_OK)
+          printf("dac0 output is set to %f volts\n", volts);
+        else
+          printf("error reading dac0");
+        errcode = SetDacOutput(0, 0, S826_DAC_SPAN_10_10, -10.00);
+
+        // X826( DemoSinewaveGenerator(board)  );      // analog sinewave output
+        // X826( TestDacRW(board) );
+        if (GetDacOutput(0, 0, &volts) == S826_ERR_OK)
+          printf("dac0 output is set to %f volts\n", volts);
+        else
+          printf("error reading dac0");
+
+
+    }
+
+    switch (errcode)
+    {
+    case S826_ERR_OK:           break;
+    case S826_ERR_BOARD:        printf("Illegal board number"); break;
+    case S826_ERR_VALUE:        printf("Illegal argument"); break;
+    case S826_ERR_NOTREADY:     printf("Device not ready or timeout"); break;
+    case S826_ERR_CANCELLED:    printf("Wait cancelled"); break;
+    case S826_ERR_DRIVER:       printf("Driver call failed"); break;
+    case S826_ERR_MISSEDTRIG:   printf("Missed adc trigger"); break;
+    case S826_ERR_DUPADDR:      printf("Two boards have same number"); break;S826_SafeWrenWrite(board, 0x02);
+    case S826_ERR_BOARDCLOSED:  printf("Board not open"); break;
+    case S826_ERR_CREATEMUTEX:  printf("Can't create mutex"); break;
+    case S826_ERR_MEMORYMAP:    printf("Can't map board"); break;
+    default:                    printf("Unknown error"); break;
+    }
+
+    
+#ifndef _LINUX  
+    printf("\nKeypress to exit ...\n\n");
+    while (!_kbhit());
+    _getch();
+#endif
+
+    S826_SystemClose();
+    return 0;
+}
+
+
+
+//////////////////////////////////
---
+++ telerob826.hpp
@@ -0,0 +1,38 @@
+#ifndef TEL826_H
+#define TEL826_H
+/*****************************************************
+type.h  (requires opencv and spinlib library)  defines matrix types: 
+
+    Inherits:
+		Flycapture
+		Opencv
+Ver 1.0 by Ashkan July-2019		
+*****************************************************/
+#include <chrono>
+#include "826api.h"
+#include <thread>
+
+
+class Telerob826{
+	private:
+	    /////////////////////////////////////////////
+		// int GrabNextImageByTrigger(INodeMap& nodeMap, CameraPtr pCam);
+		// int ResetTrigger(INodeMap& nodeMap);
+		// int ConfigureTrigger(INodeMap& nodeMap);
+	public:
+		Telerob826();
+		~Telerob826();
+		Telerob826(); 
+		int Initial();
+		void PrintCardInfo();
+		void UpdateD2A(); //Updates the frame
+		void SetD2A();
+		cv::Mat GetCurrentD2A();	//Returns the current frame from the camera (Type : opencv Mat)
+		cv::Mat GetCurrentA2D();	//Returns the current frame from the camera (Type : opencv Mat)
+		cv::Mat FilterSetup();	//Returns the current frame from the camera (Type : opencv Mat)
+		cv::Mat FilterSignal();
+		cv::Point GetTargetPose();
+		void SaveCurrentFrame(std::string fileName); // Updates and saves the current frame to the filename. You can draw the coordiante for the marker and also add the markers ID for ALL OF THE IDs.   
+		void FilterCurrentFrame(); // This is a place holder for the filter process if case you want to implement something later   
+};
+#endif // TEL826_H
