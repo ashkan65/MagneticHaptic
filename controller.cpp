afe925734c87129effb2212ce81095005c72d556
ashkan65 <ashkan.pourkand@gmail.com>
2020-11-03 14:20:28 -0700

    Adding the controller

--- controller.cpp
+++ controller.cpp
@@ -1,1 +1,27 @@
 #include "controller.hpp"
+
+
+void Controller::Init(vec_current* Amp_I_lim, current* Pow_I_lim){
+
+};
+void Controller::PINV(vec_wrench* output, Mat_actuation* A, vec_current* I_output){
+
+};
+void Controller::WPINV(vec_wrench* output, Mat_actuation* A, vec_temp_C* coils_temp, vec_current* I_output){
+
+};
+void Controller::RWPINV(vec_wrench* output, Mat_actuation* A, vec_temp_C* coils_temp, vec_current* I_output){
+
+};
+void Controller::AMPScale(){
+
+};
+void Controller::AmplifierScale(){
+
+};
+void Controller::PowerSupplyScale(){
+
+};
+void Controller::WeightMatGen(){
+
+};
