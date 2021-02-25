bc4238b91cda4a4e23c50f0abcd30c7a6a75a0d7
ashkan65 <ashkan.pourkand@gmail.com>
2021-02-24 22:35:04 -0700

    Fixing main.cpp

--- vision/main.cpp
+++ vision/main.cpp
@@ -1,8 +1,16 @@
 #include "TattileCamera.hpp"
 #include <iostream>
 
+bool cam1_switch = true;  
+ROI_t cam1_roi;
+std::atomic<short> cam1_available_index (0);
+std::atomic<bool> cam1_NewFrame (false);
+cv::Mat cam1_Buffer[3];
+
 int main() {
 	int a;
+
+
 	TattileCamera Cam1;
 	Cam1.SetupIPAddress("192.168.1.6");
 	return 0;
